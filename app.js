ffeb2de6156a0a6956ba71807cbdcee16fe4674f
Dennis <dennis.klimenkov9@gmail.com>
2020-08-18 23:07:00 -0400

    added sendGrid API for the form

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const sendGrid = require('@sendGrid/mail');
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+app.get('/api', (req, res, next) => {
+    res.send('API Status: Hello World')
+});
+
+
+app.post('/api/email', (req, res, next) => {
+
+    console.log(req.body);
+
+    sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'klimenkoff9@gmail.com',
+        from: "bccompsciclub@no-reply.company",
+        subject: 'Website Contact',
+        text: req.body.name + "\n \n" + req.body.email + "\n \n" + req.body.message
+    }
+
+    sendGrid.send(msg)
+        .then(result => {
+
+            res.status(200).json({
+                success: true
+            });
+
+        })
+        .catch(err => {
+
+            console.log('error: ', err);
+            res.status(401).json({
+                success: false
+            });
+
+        });
+});
+
+
+app.listen(3030, '0.0.0.0');
