9382905ae57d08bb2ed06862d14e17c3c2a95fe3
Kevin Tam <kevin@kevintamcs.com>
2020-12-21 00:12:42 +0000

    Add "Reply To" feature for emails and clean up code

--- app.js
+++ app.js
@@ -1,59 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const sendGrid = require("@sendgrid/mail");
 
-const sendGrid = require('@sendgrid/mail');
-
-
+// Initialize Express
 const app = express();
-
-
-app.use(bodyParser.json());
-
+app.use(express.json());
 app.use(cors());
-
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Change later to only allow our server
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
+// Email API endpoint
+app.post("/api/email", (req, res) => {
+  sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "contact@bccompsci.club",
+    from: "bccompsciclub@no-reply.company",
+    replyTo: req.body.email,
+    subject: req.body.name + " asked a question",
+    text: req.body.name + "\n \n" + req.body.email + "\n \n" + req.body.message,
+  };
 
-app.get('/api', (req, res, next) => {
-    res.send('API Status: Hello World')
+  sendGrid
+    .send(msg)
+    .then((result) => {
+      res.json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.error("error: ", err);
+      res.status(401).json({
+        success: false,
+      });
+    });
 });
-
-
-app.post('/api/email', (req, res, next) => {
-
-    console.log(req.body);
-
-    sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: 'bc.cis.club@gmail.com',
-        from: "bccompsciclub@no-reply.company",
-        subject: req.body.name + ' asked a question in Contact Form',
-        text: req.body.name + "\n \n" + req.body.email + "\n \n" + req.body.message
-    }
-
-    sendGrid.send(msg)
-        .then(result => {
-
-            res.status(200).json({
-                success: true
-            });
-
-        })
-        .catch(err => {
-
-            console.log('error: ', err);
-            res.status(401).json({
-                success: false
-            });
-
-        });
-});
-
 
 app.listen(process.env.PORT || 8080);
+console.log("Contact form service started.");
